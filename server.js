031a62f2eccf72f62be975ba08a1e3d768ba6e30
ManhAnNguyen <thungantn1999@gmail.com>
2023-12-30 10:41:11 +0700

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("Ready");
+});
+
+app.listen(3000, () => {
+  console.log("server running in port 3000");
+});
